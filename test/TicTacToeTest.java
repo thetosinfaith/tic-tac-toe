2944c1b7d629b7b520615815430cdf093f09deda
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2023-10-22 10:00:00 +0100

    run checks

--- test/TicTacToeTest.java
+++ test/TicTacToeTest.java
@@ -3,20 +3,59 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicTacToeTest {
-
     @Test
     void testGameBoardStartsEmpty(){
         TicTacToe ticTacToe = new TicTacToe();
-        boolean game = ticTacToe.gameBoard();
-        assertTrue(game);
+        boolean empty = ticTacToe.empty();
+        assertTrue(empty);
     }
     @Test
-    void testTwoPlayersCanPlayTheGame(){
+    void testTwoPlayersCanPlayTheGame() {
         TicTacToe ticTacToe = new TicTacToe();
         String userName1 = ticTacToe.firstPlayerName("Chibuzo");
         String userName2 = ticTacToe.secondPlayerName("Kimberly");
         assertNotEquals(userName1, userName2);
         assertNotEquals(userName2, userName1);
+    }
+    @Test
+    void testPlayerCanPlayInAnyDirection(){
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.playerCanPlayInAnyDirection();
+        Position userChoose = Position.X;
+        if (d )
+        Direction moveUp = Direction.NORTH;
+        Direction moveDown = Direction.WEST;
+        Direction turnRight = Direction.SOUTH;
+        Direction turnLeft = Direction.EAST;
+        assertTrue(userChoose);
+
+
+
+
+        assertTrue();
+
+        switch(userChoose){
+
+            case EMPTY :
+                Direction userChoose1 = Direction.NORTH;
+                Direction u
+                break;
+
+            case :
+
+        }
+
+
+
+        Position position(ticTacToe);
+
+        switch (ticTacToe){
+
+        }
+        ticTacToe.FirstPlayerPla
+        
+
+
 
 
 //    void testAllowTwoHumanPlayers() {
